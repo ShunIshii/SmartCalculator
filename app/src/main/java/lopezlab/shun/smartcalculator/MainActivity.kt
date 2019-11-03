c269a3314e916a7ff78068d0ba07c16a4678fe2a
Shun Ishii <ishiishun10@gmail.com>
2019-11-03 16:15:48 +0200

    Remove global variable to prepare for reclusive.

--- app/src/main/java/lopezlab/shun/smartcalculator/MainActivity.kt
+++ app/src/main/java/lopezlab/shun/smartcalculator/MainActivity.kt
@@ -12,9 +12,6 @@ class MainActivity : AppCompatActivity() {
 
     private var formula = ""
 
-    private lateinit var numList: ArrayList<Double>
-    private lateinit var opeList: ArrayList<Char>
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -100,45 +97,23 @@ class MainActivity : AppCompatActivity() {
             formulaText.text = "0"
         }
         equal.setOnClickListener {
-            formulaText.text = calculate()
+            formula = calculate(formula)
+            formulaText.text = formula
         }
     }
 
-    private fun calculate(): String {
-        if (!createElementsList()) {
-            return formula
-        }
-
-        power()
-        timesDiv()
-        plusMinus()
-
-        if (ceil(numList[0]) == floor(numList[0])) {
-            formula = numList[0].toInt().toString()
-            return numList[0].toInt().toString()
-        }
-        else {
-            formula = numList[0].toString()
-            return numList[0].toString()
-        }
-    }
-
-    private fun createElementsList(): Boolean {
-        numList = ArrayList()
-        opeList = ArrayList()
-        var strnum = ""
+    private fun checkFormula(str: String): Boolean {
         var isNum = false
         var isDecimal = false
         var isPower = false
         val toast = Toast.makeText(applicationContext, "Invalid formula!", Toast.LENGTH_SHORT)
 
-        for (c in formula) {
+        for (c in str) {
             if (!isNum && c !in '0'..'9') {
                 toast.show()
                 return false
             }
             if (c in '0'..'9' || c == '.') {
-                strnum += c
                 isNum = true
                 if (c == '.') {
                     if (isDecimal || isPower) {
@@ -152,18 +127,53 @@ class MainActivity : AppCompatActivity() {
             }
             else {
                 isPower = c == '^'
-                opeList.add(c)
-                numList.add(strnum.toDouble())
-                strnum = ""
                 isNum = false
                 isDecimal = false
             }
         }
-        numList.add(strnum.toDouble())
         return true
     }
 
-    private fun power() {
+    private fun calculate(str: String): String {
+        if (!checkFormula(str)) {
+            return str
+        }
+
+        val (numList, opeList) = createElementsList(str)
+
+        val (numLis, opeLis) = power(numList, opeList)
+
+        val (numLi, opeLi) = timesDiv(numLis, opeLis)
+
+        val ans = plusMinus(numLi, opeLi)
+
+        return if (ceil(ans) == floor(ans)) {
+            ans.toInt().toString()
+        } else {
+            ans.toString()
+        }
+    }
+
+    private fun createElementsList(str: String): Pair<ArrayList<Double>, ArrayList<Char>> {
+        val numList = ArrayList<Double>()
+        val opeList = ArrayList<Char>()
+        var strnum = ""
+
+        for (c in str) {
+            if (c in '0'..'9' || c == '.') {
+                strnum += c
+            }
+            else {
+                opeList.add(c)
+                numList.add(strnum.toDouble())
+                strnum = ""
+            }
+        }
+        numList.add(strnum.toDouble())
+        return Pair(numList, opeList)
+    }
+
+    private fun power(numList: ArrayList<Double>, opeList: ArrayList<Char>): Pair<ArrayList<Double>, ArrayList<Char>> {
         var i = 0
         while (i < opeList.size) {
             if (opeList[i] == '^') {
@@ -179,47 +189,50 @@ class MainActivity : AppCompatActivity() {
                 i++
             }
         }
+        return Pair(numList, opeList)
     }
 
-    private fun timesDiv() {
+    private fun timesDiv(numList: ArrayList<Double>, opeList: ArrayList<Char>): Pair<ArrayList<Double>, ArrayList<Char>> {
         var i = 0
         while (i < opeList.size) {
-            if (opeList[i] == '×') {
-                var b1: BigDecimal = BigDecimal.valueOf(numList[i])
-                var b2: BigDecimal = BigDecimal.valueOf(numList[i+1])
-                numList[i] = b1.times(b2).toDouble()
-                numList.removeAt(i + 1)
-                opeList.removeAt(i)
-            }
-            else if (opeList[i] == '÷') {
-                var b1: BigDecimal = BigDecimal.valueOf(numList[i])
-                var b2: BigDecimal = BigDecimal.valueOf(numList[i+1])
-                numList[i] = b1.div(b2).toDouble()
-                numList.removeAt(i + 1)
-                opeList.removeAt(i)
-            }
-            else {
-                i++
+            when {
+                opeList[i] == '×' -> {
+                    val b1: BigDecimal = BigDecimal.valueOf(numList[i])
+                    val b2: BigDecimal = BigDecimal.valueOf(numList[i+1])
+                    numList[i] = b1.times(b2).toDouble()
+                    numList.removeAt(i + 1)
+                    opeList.removeAt(i)
+                }
+                opeList[i] == '÷' -> {
+                    val b1: BigDecimal = BigDecimal.valueOf(numList[i])
+                    val b2: BigDecimal = BigDecimal.valueOf(numList[i+1])
+                    numList[i] = b1.div(b2).toDouble()
+                    numList.removeAt(i + 1)
+                    opeList.removeAt(i)
+                }
+                else -> i++
             }
         }
+        return Pair(numList, opeList)
     }
 
-    private fun plusMinus () {
+    private fun plusMinus (numList: ArrayList<Double>, opeList: ArrayList<Char>): Double {
         for (j in 0 until opeList.size) {
             if (opeList[0] == '+') {
-                var b1: BigDecimal = BigDecimal.valueOf(numList[0])
-                var b2: BigDecimal = BigDecimal.valueOf(numList[1])
+                val b1: BigDecimal = BigDecimal.valueOf(numList[0])
+                val b2: BigDecimal = BigDecimal.valueOf(numList[1])
                 numList[0] = b1.plus(b2).toDouble()
                 numList.removeAt(1)
                 opeList.removeAt(0)
             }
             else if (opeList[0] == '-') {
-                var b1: BigDecimal = BigDecimal.valueOf(numList[0])
-                var b2: BigDecimal = BigDecimal.valueOf(numList[1])
+                val b1: BigDecimal = BigDecimal.valueOf(numList[0])
+                val b2: BigDecimal = BigDecimal.valueOf(numList[1])
                 numList[0] = b1.minus(b2).toDouble()
                 numList.removeAt(1)
                 opeList.removeAt(0)
             }
         }
+        return numList[0]
     }
 }
