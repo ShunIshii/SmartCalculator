aedb6df979f432c3bfe348fb0fae5844eb4b82b6
Shun Ishii <ishiishun10@gmail.com>
2019-11-05 23:41:57 +0200

    Added an ad.

--- app/src/main/java/lopezlab/shun/smartcalculator/MainActivity.kt
+++ app/src/main/java/lopezlab/shun/smartcalculator/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import com.google.android.gms.ads.*
 import kotlinx.android.synthetic.main.activity_main.*
 import java.math.BigDecimal
 import kotlin.math.ceil
@@ -14,12 +15,44 @@ class MainActivity : AppCompatActivity() {
     private var formula = ""
     private val TAG = "MainActivity"
     private var parenthesisCount = 0
+    lateinit var mAdView : AdView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         formulaText.text = "0"
         initButtons()
+        MobileAds.initialize(this) {}
+        mAdView = findViewById(R.id.adView)
+        val adRequest = AdRequest.Builder().build()
+        mAdView.loadAd(adRequest)
+        mAdView.adListener = object: AdListener() {
+            override fun onAdLoaded() {
+                // Code to be executed when an ad finishes loading.
+            }
+
+            override fun onAdFailedToLoad(errorCode : Int) {
+                // Code to be executed when an ad request fails.
+            }
+
+            override fun onAdOpened() {
+                // Code to be executed when an ad opens an overlay that
+                // covers the screen.
+            }
+
+            override fun onAdClicked() {
+                // Code to be executed when the user clicks on an ad.
+            }
+
+            override fun onAdLeftApplication() {
+                // Code to be executed when the user has left the app.
+            }
+
+            override fun onAdClosed() {
+                // Code to be executed when the user is about to return
+                // to the app after tapping on an ad.
+            }
+        }
     }
 
     private fun initButtons() {
